62edb742ffb3340a2a6136f25cc745bf900ca0a7
Thanoj Singh <thanoj.singh@gmail.com>
2024-12-02 09:43:04 -0500

    better day2

--- day2/part2/main.go
+++ day2/part2/main.go
@@ -10,9 +10,13 @@ import (
 
 // some notes about this day 2, it took an hour to solve and part 2 took a while
 // because I made an assumption that you can be greedy and just remove one of
-// the first two levels that causes the report to be bad.  That doesn't seem to
+// the first two levels that causes the report to be bad.  that doesn't seem to
 // be the case for some reason and I ended up brute forcing to get the answer
 // quicker.
+//
+// missed the fact that the first level can be removed to create a report that is
+// increasing instead of decreasing and vice versa. this means you only have to
+// check 3 different reports not all of them
 func main() {
 	b, err := os.ReadFile("day_2_input.txt")
 	if err != nil {
@@ -61,19 +65,11 @@ func isReportSafe(report []int, tolerant bool) bool {
 				return false
 			}
 
-			// this checks all possible reports, but it seems like too much of a
-			// brute force solution to be the most opitmal way to do it.
-			bools := []bool{}
-			for i := 0; i < len(report); i++ {
-				newReport := slices.Concat(report[:i], report[i+1:])
-				bools = append(bools, isReportSafe(newReport, false))
-			}
-			for _, b := range bools {
-				if b {
-					return true
-				}
-			}
-			return false
+			newReport1 := slices.Concat(report[:i], report[i+1:])
+			newReport2 := slices.Concat(report[:j], report[j+1:])
+			newReport3 := report[1:]
+
+			return isReportSafe(newReport1, false) || isReportSafe(newReport2, false) || isReportSafe(newReport3, false)
 		}
 	}
 	return true
