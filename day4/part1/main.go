c8ebeaa44ad9f07d884093015491de3d43b6da55
Thanoj Singh <thanoj.singh@gmail.com>
2024-12-04 15:47:18 -0500

    day 4

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -6,6 +6,11 @@ import (
 	"strings"
 )
 
+type pair struct {
+	i int
+	j int
+}
+
 func main() {
 	b, err := os.ReadFile("day_4_input.txt")
 	if err != nil {
@@ -27,5 +32,63 @@ func main() {
 			grid[i][j] = r
 		}
 	}
-	fmt.Println(grid)
+
+	sum := 0
+	for i, row := range grid {
+		for j, _ := range row {
+			sum += check(grid, i, j)
+		}
+	}
+	fmt.Println(sum)
+}
+
+func concat(texts ...string) string {
+	return strings.Join(texts, "")
+}
+
+func check(grid [][]rune, i, j int) int {
+	words := []string{}
+	total := 0
+
+	directions := []pair{
+		{-1, -1},
+		{-1, 0},
+		{-1, 1},
+		{0, 1},
+		{1, 1},
+		{1, 0},
+		{1, -1},
+		{0, -1},
+	}
+
+	for _, direction := range directions {
+		words = append(words,
+			concat(
+				get(grid, i, j),
+				get(grid, i+(1*direction.i), j+(1*direction.j)),
+				get(grid, i+(2*direction.i), j+(2*direction.j)),
+				get(grid, i+(3*direction.i), j+(3*direction.j)),
+			),
+		)
+	}
+
+	for _, word := range words {
+		if word == "XMAS" {
+			total++
+		}
+	}
+	return total
+}
+
+func get(grid [][]rune, i, j int) string {
+	if i < 0 || i >= len(grid) {
+		return ""
+	}
+	row := grid[i]
+
+	if j < 0 || j >= len(row) {
+		return ""
+	}
+
+	return string(row[j])
 }
--- day4/part2/main.go
+++ day4/part2/main.go
@@ -3,57 +3,87 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
 	"strings"
 )
 
+type pair struct {
+	i int
+	j int
+}
+
 func main() {
-	b, err := os.ReadFile("day_3_input.txt")
+	b, err := os.ReadFile("day_4_input.txt")
 	if err != nil {
 		panic(err)
 	}
+
 	input := string(b)
 	lines := strings.Split(input, "\n")
+	lines = lines[0 : len(lines)-1]
+	lineLength := len(lines[0])
 
-	enabled := true
+	grid := make([][]rune, len(lines))
+	for i := range grid {
+		grid[i] = make([]rune, lineLength)
+	}
+
+	for i, line := range lines {
+		for j, r := range line {
+			grid[i][j] = r
+		}
+	}
+
 	sum := 0
-	for _, line := range lines {
-		memory := line
-		re := regexp.MustCompile(`(mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\))`)
-
-		matches := re.FindAllStringSubmatch(memory, -1)
-
-		for _, match := range matches {
-			instruction := match[0]
-
-			if instruction == "do()" {
-				enabled = true
-				continue
-			}
-			if instruction == "don't()" {
-				enabled = false
-				continue
-			}
-
-			opPart := instruction[4 : len(instruction)-1]
-			opParts := strings.Split(opPart, ",")
-
-			op1s, op2s := opParts[0], opParts[1]
-			op1, err := strconv.Atoi(op1s)
-			if err != nil {
-				panic(err)
-			}
-			op2, err := strconv.Atoi(op2s)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Printf("%d * %d\n", op1, op2)
-			if enabled {
-				sum += (op1 * op2)
+	for i, row := range grid {
+		for j, _ := range row {
+			if check(grid, i, j) {
+				sum++
 			}
 		}
 	}
 	fmt.Println(sum)
+}
+
+func concat(texts ...string) string {
+	return strings.Join(texts, "")
+}
+
+func check(grid [][]rune, i, j int) bool {
+	words := []string{}
+	total := 0
+
+	words = append(words,
+		concat(
+			get(grid, i-1, j-1),
+			get(grid, i, j),
+			get(grid, i+1, j+1),
+		),
+	)
+	words = append(words,
+		concat(
+			get(grid, i+1, j-1),
+			get(grid, i, j),
+			get(grid, i-1, j+1),
+		),
+	)
+
+	for _, word := range words {
+		if word == "MAS" || word == "SAM" {
+			total++
+		}
+	}
+	return total == 2
+}
+
+func get(grid [][]rune, i, j int) string {
+	if i < 0 || i >= len(grid) {
+		return ""
+	}
+	row := grid[i]
+
+	if j < 0 || j >= len(row) {
+		return ""
+	}
+
+	return string(row[j])
 }
