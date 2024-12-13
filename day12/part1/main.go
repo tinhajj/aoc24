3f3be461d5fcf5f68944ea2e3d0ef2d8ab1e8489
Thanoj Singh <thanoj.singh@gmail.com>
2024-12-12 21:00:43 -0500

    line feed change

--- day12/part1/main.go
+++ day12/part1/main.go
@@ -17,12 +17,44 @@ func main() {
 	input := string(b)
 	lines := strings.Split(input, "\n")
 	lines = lines[:len(lines)-1]
-	m, _, _ := scan.RuneMatrix(lines)
+	m, height, width := scan.RuneMatrix(lines)
 	matrix := st.VertexMatrixStr(m)
 
-	for _, row := range matrix {
-		for _, vert := range row {
-			fmt.Println(vert)
+	adjMap := map[*st.VertexStr][]*st.VertexStr{}
+
+	for i, row := range matrix {
+		for j, vert := range row {
+			directions := []st.Point{
+				{Y: -1, X: 0},
+				{Y: 0, X: 1},
+				{Y: 1, X: 0},
+				{Y: 0, X: -1},
+			}
+
+			adjacents := []*st.VertexStr{}
+			for _, direction := range directions {
+				ii := i + direction.Y
+				jj := j + direction.X
+
+				if ii >= 0 && ii < height && jj >= 0 && jj < width {
+					adjacents = append(adjacents, matrix[ii][jj])
+				}
+			}
+
+			for _, adjacent := range adjacents {
+				adjMap[vert] = append(adjMap[vert], adjacent)
+			}
 		}
+	}
+
+	for k, v := range adjMap {
+		fmt.Printf("Vertex: %+v\n", k.Point)
+
+		fmt.Printf("Adjacents: ")
+		for _, other := range v {
+			fmt.Printf("%+v ", other.Val)
+		}
+		fmt.Println("")
+		fmt.Println("")
 	}
 }
