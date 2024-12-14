de7787a102ec60766d23882e1267ebaffd8afd15
Thanoj Singh <thanoj.singh@gmail.com>
2024-12-14 00:41:24 -0500

    day 12, still working through

--- day12/part1/main.go
+++ day12/part1/main.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+type 
+
 func main() {
 	b, err := os.ReadFile("day_12_sample_input.txt")
 	if err != nil {
@@ -48,7 +50,7 @@ func main() {
 	}
 
 	for k, v := range adjMap {
-		fmt.Printf("Vertex: %+v\n", k.Point)
+		fmt.Printf("Vertex: (%+v) %+v\n", k.Point, k.Val)
 
 		fmt.Printf("Adjacents: ")
 		for _, other := range v {
@@ -57,4 +59,68 @@ func main() {
 		fmt.Println("")
 		fmt.Println("")
 	}
+
+	corner := matrix[0][0]
+
+	regions := map[string]int{}
+
+	found, area, perimeter := bfs(height, width, adjMap, corner)
+	fmt.Println(found, area, perimeter)
+	// for _, v := range found {
+	// 	fmt.Printf("%+v ", v)
+	// }
+	fmt.Println()
+}
+
+func bfs(matrixH, matrixW int, adjMap map[*st.VertexStr][]*st.VertexStr, start *st.VertexStr) (found []*st.VertexStr, a, p int) {
+	a = 0
+	p = 0
+
+	var queue []*st.VertexStr
+	visited := make(map[*st.VertexStr]bool)
+
+	visited[start] = true
+	queue = append(queue, start)
+
+	for len(queue) > 0 {
+		next := queue[0]
+		queue = queue[1:]
+
+		if next.Val == start.Val {
+			a++
+		}
+		if next.Point.X == 0 {
+			p++
+		}
+		if next.Point.Y == 0 {
+			p++
+		}
+		if next.Point.X == matrixW-1 {
+			p++
+		}
+		if next.Point.Y == matrixH-1 {
+			p++
+		}
+
+		for _, neighbour := range adjMap[next] {
+			if ok := visited[neighbour]; !ok {
+				if neighbour.Val != start.Val {
+					p++
+					continue
+				}
+
+				visited[neighbour] = true
+				queue = append(queue, neighbour)
+			}
+		}
+	}
+
+	found = []*st.VertexStr{}
+	for k, _ := range visited {
+		if k.Val == start.Val {
+			found = append(found, k)
+		}
+	}
+
+	return found, a, p
 }
